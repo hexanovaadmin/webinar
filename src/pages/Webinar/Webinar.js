f33bc574542ee260975d923ba06f2895af83dd81
Saheb Dhara <sahebdhara961@gmail.com>
2023-10-11 00:11:55 +0530

    first commit

---
+++ src/pages/Webinar/Webinar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import WebinarAdminCreateWebinarTab from '../../components/WebinarAdminContent/WebinarAdminCreateWebinarTab'
+import WebinarAdminWebinarListTab from '../../components/WebinarAdminContent/WebinarAdminWebinarListTab'
+import WebinarAdminContent from '../../components/WebinarAdminContent/WebinarAdminContent '
+
+
+const Webinar = () => {
+  return (
+    <div>
+        <WebinarAdminContent/>
+    </div>
+  )
+}
+
+export default Webinar
