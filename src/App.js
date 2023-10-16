d246a3be8df8a21d0c51ca29775d0c7b3aaba81a
Saheb Dhara <87798754+SAHEBDHARA@users.noreply.github.com>
2023-10-16 23:20:30 +0530

    adding side navigation bar

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 
 import "./App.scss";
 import Webinar from "./pages/Webinar/Webinar";
+import ChatNavigation from './Components/ChatNavigation/ChatNavigation'
 
 function App() {
   return (
     <div>
-      <Webinar />
+      <ChatNavigation/>
     </div>
   );
 }
