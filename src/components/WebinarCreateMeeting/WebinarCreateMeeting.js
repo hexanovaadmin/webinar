370008f944ff0f8b8b5f5cb0c95a270c6513e33c
sunnyaditya07 <sunnyadityaa@gmail.com>
2023-11-09 11:15:35 +0530

    meeting

--- src/components/WebinarCreateMeeting/WebinarCreateMeeting.js
+++ src/components/WebinarCreateMeeting/WebinarCreateMeeting.js
@@ -1,4 +1,5 @@
 import "./WebinarCreateMeeting.scss";
+import axios from "axios";
 import {
   VideoTileGrid,
   useMeetingManager,
@@ -9,52 +10,54 @@ import { useState } from "react";
 const WebinarCreateMeeting = () => {
   const meetingManager = useMeetingManager();
   const [meeting, setMeeting] = useState(false);
+
   const joinMeeting = async () => {
     try {
-      const meetingDetails = {
-        externalMeetingId:
-          "ir4u-external-meet-id-7d662294-2ae9-4cba-985f-f44920b9abdd",
-        mediaPlacement: {
-          audioFallbackUrl:
-            "wss://haxrp.m2.ue1.app.chime.aws:443/calls/5cbf74f4-6a2d-449f-a773-08836eb52713",
-          audioHostUrl:
-            "8469b5daaec55adf25a3ea8ccdcf5e22.k.m2.ue1.app.chime.aws:3478",
-          eventIngestionUrl:
-            "https://data.svc.ue1.ingest.chime.aws/v1/client-events",
-          screenDataUrl:
-            "wss://bitpw.m2.ue1.app.chime.aws:443/v2/screen/5cbf74f4-6a2d-449f-a773-08836eb52713",
-          screenSharingUrl:
-            "wss://bitpw.m2.ue1.app.chime.aws:443/v2/screen/5cbf74f4-6a2d-449f-a773-08836eb52713",
-          screenViewingUrl:
-            "wss://bitpw.m2.ue1.app.chime.aws:443/ws/connect?passcode=null&viewer_uuid=null&X-BitHub-Call-Id=5cbf74f4-6a2d-449f-a773-08836eb52713",
-          signalingUrl:
-            "wss://signal.m2.ue1.app.chime.aws/control/5cbf74f4-6a2d-449f-a773-08836eb52713",
-          turnControlUrl:
-            "https://2713.cell.us-east-1.meetings.chime.aws/v2/turn_sessions",
-        },
-        mediaRegion: "us-east-1",
-        meetingArn:
-          "arn:aws:chime:us-east-1:077919032406:meeting/5cbf74f4-6a2d-449f-a773-08836eb52713",
-        meetingFeatures: {
-          audio: {
-            echoReduction: "UNAVAILABLE",
-          },
-        },
-        meetingHostId: "ir4u-host-d9a8a345-f28b-4f22-a42b-cb081375590b",
-        meetingId: "5cbf74f4-6a2d-449f-a773-08836eb52713",
-        primaryMeetingId: "",
-        tenantIds: null,
-      };
       const hostAttendeeDetails = {
-        attendeeId: "16adb4a6-248b-6b27-8128-181a223c57b7",
+        meetingId: "",
+        attendeeId: "21b0255f-9468-c28b-9d70-1ae0aaa01b5d",
         capabilities: {
           audio: "Send",
           content: "Send",
           video: "Send",
         },
-        externalUserId: "ir4u-user-id-9a087631-3454-4bdc-97e4-ff235c7e7067",
+        externalUserId: "ir4u-user-id-538873fa-47a5-4ada-b0b2-5a4b26eaac2f",
         joinToken:
-          "MTZhZGI0YTYtMjQ4Yi02YjI3LTgxMjgtMTgxYTIyM2M1N2I3OjhjYWMwMDJkLTZmOTAtNGNkYi05Y2I0LTY2NjhjZTBmZGVjNA",
+          "MjFiMDI1NWYtOTQ2OC1jMjhiLTlkNzAtMWFlMGFhYTAxYjVkOmRiNjFmZDc2LTMwYzItNDBjMS05NDAwLTE4MzRlNDU1N2NhMA",
+      };
+      const meetingDetails = {
+        externalMeetingId:
+          "ir4u-external-meet-id-eab9d322-23d1-471a-b8a0-d8c3e88e86f3",
+        mediaPlacement: {
+          audioFallbackUrl:
+            "wss://haxrp.m2.ue1.app.chime.aws:443/calls/43992a27-0652-4428-b0ab-6c0af9922713",
+          audioHostUrl:
+            "9a6d2ccf2018cd7dc989a4be07ce0e74.k.m2.ue1.app.chime.aws:3478",
+          eventIngestionUrl:
+            "https://data.svc.ue1.ingest.chime.aws/v1/client-events",
+          screenDataUrl:
+            "wss://bitpw.m2.ue1.app.chime.aws:443/v2/screen/43992a27-0652-4428-b0ab-6c0af9922713",
+          screenSharingUrl:
+            "wss://bitpw.m2.ue1.app.chime.aws:443/v2/screen/43992a27-0652-4428-b0ab-6c0af9922713",
+          screenViewingUrl:
+            "wss://bitpw.m2.ue1.app.chime.aws:443/ws/connect?passcode=null&viewer_uuid=null&X-BitHub-Call-Id=43992a27-0652-4428-b0ab-6c0af9922713",
+          signalingUrl:
+            "wss://signal.m2.ue1.app.chime.aws/control/43992a27-0652-4428-b0ab-6c0af9922713",
+          turnControlUrl:
+            "https://2713.cell.us-east-1.meetings.chime.aws/v2/turn_sessions",
+        },
+        mediaRegion: "us-east-1",
+        meetingArn:
+          "arn:aws:chime:us-east-1:077919032406:meeting/43992a27-0652-4428-b0ab-6c0af9922713",
+        meetingFeatures: {
+          audio: {
+            echoReduction: "UNAVAILABLE",
+          },
+        },
+        meetingHostId: "ir4u-host-bec10182-8ac8-4a73-96f3-8f97dec83b8b",
+        meetingId: "43992a27-0652-4428-b0ab-6c0af9922713",
+        primaryMeetingId: "",
+        tenantIds: null,
       };
       const meetingSessionConfiguration = new MeetingSessionConfiguration(
         meetingDetails,
